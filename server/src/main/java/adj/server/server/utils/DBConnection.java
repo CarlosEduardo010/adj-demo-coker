8d9165f82c285de81d59e1c564614ca84394bf63
CarlosEduardo010 <20223tn152@utez.edu.mx>
2025-11-08 12:31:35 -0600

    hola

--- server/src/main/java/adj/server/server/utils/DBConnection.java
+++ server/src/main/java/adj/server/server/utils/DBConnection.java
@@ -8,29 +8,32 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 
 @Configuration
-public class DBConecction {
+public class DBConnection {
 
     @Value("${db.host}")
     private String host;
+
     @Value("${db.port}")
     private String port;
+
     @Value("${db.name}")
     private String name;
-    @Value("${db.password}")
-    private String password;
+
     @Value("${db.user}")
     private String user;
 
+    @Value("${db.pass}")
+    private String pass;
+
     @Bean
     public DataSource getDBConnection() {
-
         DriverManagerDataSource source = new DriverManagerDataSource();
-        source.setDriverClassName("com.mysql.jdbc.Driver");
+        source.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        // jdbc:mysql://localhost:3306/adj-demo
         source.setUrl("jdbc:mysql://" + host + ":" + port + "/" + name);
         source.setUsername(user);
-        source.setPassword(password);
+        source.setPassword(pass);
 
         return source;
     }
-
 }
